45dcee370fe9d5fc6f5ad08d87669cc7b0cc9002
johnnybalats <westcoast1910@hotmail.com>
2017-09-08 16:07:29 +0300

    Alter Points
    Change of the points

--- src/Classes/Points.java
+++ src/Classes/Points.java
@@ -2,13 +2,13 @@ package Classes;
 
 public class Points {
 
-	public static double eachStandardClassObject = 0.15;
-	public static double eachCustomClassObject = 0.3;
+	public static double eachStandardClassObject = 0.3;
+	public static double eachCustomClassObject = 0.5;
 	public static double eachStandardAttrbite = 0.1;
 	public static double eachCustomAttribute = 0.4;
-	public static double hasAttributes = 0.2;
+	public static double hasAttributes = 0.3;
 	public static double voidMethod = 0.1;
 	public static double standardReturnType = 0.2;
-	public static double customReturnType = 0.2;
+	public static double customReturnType = 0.3;
 	public static double eachMethod = 0.1;
 }
